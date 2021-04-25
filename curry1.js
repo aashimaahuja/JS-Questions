585ff8cc6be8711f452d8cf65cf588cc26b132a5
AashimaAhuja <aashima.ahuja@booking.com>
2021-04-25 19:42:57 +0200

    Curry1

---
+++ curry1.js
@@ -0,0 +1,17 @@
+// Given a function , return curried function
+
+multiply(2, 3, 4);
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+function curry(fn) {
+  return function curried() {
+    let args = [...arguments];
+    if (args.length >= fn.length) return fn(...args);
+    return curried.bind(null, ...args);
+  };
+}
+
+let curried = curry(multiply);
+console.log(curried(2, 3)(3));
--- index.js
+++ index.js
@@ -39,3 +39,58 @@ function sum2(...args){
   return inner;
 }
 console.log(sum2(1)(2)(4)())
+
+
+// Bind Pollyfill
+Function.prototype.mybind = function(context, ...args){
+  let fn = this;
+
+  if(!(fn instanceof Function)){
+    console.log('Not an instance of function')
+    return null;
+  }
+
+  return function(...args2){
+    fn.apply(context,[...args,...args2]);
+  }
+}
+
+let obj = {
+  foo: 'bar'
+}
+
+function fn(){
+  console.log(this.foo);
+}
+
+fn.mybind(obj)();
+
+// Call Pollyfill
+
+Function.prototype.myCall = function(context, ...args){
+  context = context || window;
+  context = Object(context);
+
+  const fn = Symbol();
+  context[fn] = this;
+  let res = context[fn](...args);
+  delete context[fn];
+
+  return res;
+
+}
+
+// Apply Pollyfill
+
+Function.prototype.myCall = function(context,args){
+  context = context || window;
+  context = Object(context);
+
+  const fn = Symbol();
+  context[fn] = this;
+  let res = context[fn](...args);
+  delete context[fn];
+
+  return res;
+
+}
